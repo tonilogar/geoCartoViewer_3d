7b5408667cb31f8c7d751c26380c752f7a68d498
tonilogar <tologar@gmail.com>
2022-05-24 16:27:13 +0200

    add mapBox library

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -1,11 +1,12 @@
 
 
 
-/* import "./mapbox.js" */
+
 import "./classes/addProject/addProject.js"
 import "./classes/addRaster/addRaster.js"
 import "./classes/addvector/addVector.js"
 import "./classes/changeBackground/changeBackground.js"
+import "./mapbox.js" 
 
 
 
