efcfa3192501f437802540dc6ca4d002d302a617
tonilogar <tologar@gmail.com>
2022-08-14 17:08:33 +0200

    copy files old repositories

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
-}) 
-    .then(db => console.log(`DB is connected`))
-    .catch(err => console.error(err))
-
-    console.group('Data base ' + process.env.MONGODB_URI )
