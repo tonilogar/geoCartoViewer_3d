77d9ea4f18b4ec89fe1b0e92d5a5573d2d72c67c
tonilogar <tologar@gmail.com>
2022-11-18 11:24:32 +0100

    Update projectsService.js

--- backend/public/js/classes/projectsService/projectsService.js
+++ backend/public/js/classes/projectsService/projectsService.js
@@ -1,7 +1,7 @@
 class ProjectsService {
 
   constructor() {
-    this.URI = `http://geocartoviewer_3d/api/projects`;
+    this.URI = `http://localhost:4000/api/projects`
     this.projectById
     this.projectsDataBaseById
     this.projectsDataBase
@@ -24,7 +24,7 @@ class ProjectsService {
   }
 
   async getPro() {
-    const URL = 'http://geocartoviewer_3d/api/projects'
+    const URL = `http://localhost:4000/api/projects`
     await fetch(URL)
       .then(res => res.json())
       .then(data => {
