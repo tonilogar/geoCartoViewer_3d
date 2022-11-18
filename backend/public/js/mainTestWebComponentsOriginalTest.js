b729e0ed0fdb726d425851aa3386780f70ea82e6
tonilogar <tologar@gmail.com>
2022-11-18 11:22:59 +0100

    Update mainTestWebComponentsOriginalTest.js

--- backend/public/js/mainTestWebComponentsOriginalTest.js
+++ backend/public/js/mainTestWebComponentsOriginalTest.js
@@ -7,24 +7,36 @@ import { CreateLayersContainer } from './classes/createLayers/createLayers.js'
 
 
 const projectsMenu = new ProjectsMenu()
+const createLayersContainer = new CreateLayersContainer()
 projectsMenu.renderProjectsMenu()
 
-const createLayersContainer = new CreateLayersContainer()
 
 
 
 
-
-window.onload = () => {
-
+//setTimeout(projectsMenu.renderProjectsMenu(), 3000)
+projectsMenu.renderProjectsMenu()
+setTimeout(selectProject, 1000)
+function selectProject(){
   const options = document.querySelectorAll('.select')
   for (var i = 0; i < options.length; i++) {
     options[i].addEventListener("change", (e) => {
       createLayersContainer.renderLayersContainer(projectsMenu.showData())
     })
-    
   }
+}
+//map.setStyle('mapbox://styles/mapbox/satellite-v9')
+//map.setStyle('http://betaserver.icgc.cat/vectortiles/estils/niceredtopo.json')
+//map.setStyle('http://betaserver.icgc.cat/vectortiles/estils/bt5mbase_pla.json')
+//map.setStyle('http://betaserver.icgc.cat/vectortiles/estils/redtopoestil.json')
+//map.setStyle('http://betaserver.icgc.cat/vectortiles/estils/bt5m_nice.json')
+//map.setStyle('http://betaserver.icgc.cat/vectortiles/estils/altimbt5m.json')
+map.setStyle('https://geoserveis.icgc.cat/contextmaps/icgc_geologic_riscos.json')
+
+
+/* window.onload = () => {
+
   
   
 }
-
+ */
